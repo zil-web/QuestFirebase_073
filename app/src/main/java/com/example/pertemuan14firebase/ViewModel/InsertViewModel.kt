fc665dab5825aa5fe8b642711b863c79208dfdd5
zil-web <126854462+zil-web@users.noreply.github.com>
2025-01-13 14:04:40 +0700

    menampilkan daftar mahasiswa (Mahasiswa objects) yang diambil dari HomeViewModel

---
+++ app/src/main/java/com/example/pertemuan14firebase/ViewModel/InsertViewModel.kt
@@ -0,0 +1,115 @@
+package com.example.pertemuan14firebase.ViewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan14firebase.model.Mahasiswa
+import com.example.pertemuan14firebase.repository.MahasiswaRepository
+
+import kotlinx.coroutines.launch
+
+class InsertViewModel (
+    private val mhs: MahasiswaRepository
+) : ViewModel() {
+
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+
+    //Validasi data input pengguna
+    fun validateFields() : Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenis_kelamin = if (event.jenis_kelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun insertMhs() {
+        if (validateFields()) {
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMahasiswa(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data berhasil disimpan")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Data gagal disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data tidak valid")
+        }
+    }
+
+    fun resetForm() {
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+
+    fun resetSnackBarMessage() {
+        uiState = FormState.Idle
+    }
+}
+
+sealed class FormState {
+    object Idle: FormState()
+    object Loading: FormState()
+    data class Success(val message: String): FormState()
+    data class Error(val message: String): FormState()
+}
+
+data class InsertUiState (
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState()
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenis_kelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenis_kelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
+//data class variable yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenis_kelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+//Menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa (
+    nim = nim,
+    nama = nama,
+    jenis_kelamin = jenis_kelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
--- app/src/main/java/com/example/pertemuan14firebase/model/Mahasiswa.kt
+++ app/src/main/java/com/example/pertemuan14firebase/model/Mahasiswa.kt
@@ -4,7 +4,7 @@ package com.example.pertemuan14firebase.model
 data class Mahasiswa (
     val nim: String ,
     val nama: String,
-    val jenisKelamin: String,
+    val jenis_Kelamin: String,
     val alamat: String,
     val kelas: String,
     val angkatan: String
